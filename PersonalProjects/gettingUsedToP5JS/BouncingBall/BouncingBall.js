b6e7861a3902bfdd1197539923a3c949a67e8d0a
BubbleCoding <rover.vos@live.nl>
2022-10-18 15:14:04 +0200

    I "finished" this.
    The math isn't correct, but I now understand the syntax of p5js.

--- PersonalProjects/gettingUsedToP5JS/BouncingBall/BouncingBall.js
+++ PersonalProjects/gettingUsedToP5JS/BouncingBall/BouncingBall.js
@@ -3,7 +3,7 @@ let balls = [];
 
 function setup() {
   createCanvas(400, 400);
-  for (i=0; i< 50; i++) {
+  for (i=0; i< 10; i++) {
     balls.push(new Ball());
   }
 }
@@ -13,6 +13,14 @@ function draw() {
   background(100);
   for (i=0; i< balls.length; i++) {
     balls[i].update();
+  }
+  for (i=0; i< balls.length; i++) {
+    for (j=0; j< balls.length; j++) {
+      let distance = p5.Vector.dist(balls[i].getPos(), balls[j].getPos());
+      if (distance < 20 && distance != 0) {
+        balls[i].updateSpeed(j);
+      }
+    }
   }
 }
 
@@ -27,10 +35,42 @@ class Ball {
     this.move();
     this.display();
     this.bounceWall();
+    this.bounceBall();
   }
-
   getPos() {
     return this.pos;
+  }
+  getPosX() {
+    return this.pos.x;
+  }
+
+  getPosY() {
+    return this.pos.y;
+  }
+
+  updateSpeed(other) {
+    this.speed.x = -1 * this.speed.x;
+    this.speed.y = -1 * this.speed.y;
+
+    let minDist = 20;
+    let spring = 0.05;
+    let dx = balls[other].pos.x - this.pos.x;
+    let dy = balls[other].pos.y - this.pos.y;
+
+
+    let angle = atan2(dy, dx);
+    let targetX = this.pos.x + cos(angle) * minDist;
+    let targetY = this.pos.y + sin(angle) * minDist;
+    let ax = (targetX - balls[other].getPosX()) * spring;
+    let ay = (targetY - balls[other].getPosY()) * spring;
+    this.speed.x -= ax;
+    this.speed.y -= ay;
+    balls[other].changeSpeed(ax, ay);
+  }
+
+  changeSpeed(ax, ay) {
+    this.speed.x += ax;
+    this.speed.y += ay;
   }
 
   move() {
